44a5970766707d47c7f6a1c8bd66218227459c25
hadiSahin <81286335+hadiSahin@users.noreply.github.com>
2025-05-09 10:25:03 -0400

    Update node.py

--- node.py
+++ node.py
@@ -3,11 +3,17 @@
 # import config
 
 class Node(object):
-	def __init__(self, Id, w, W, parent, splits):
+	def __init__(self, Id, w, W, parent, sybil, winit, Winit, group, gW, gSize, rep):
 		self.nodeId = Id
 		self.w = w
 		self.W = W
 		self.parent = parent
-		self.splits = splits
+		self.sybil = sybil
+		self.winit = winit
+		self.Winit = Winit
+		self.group = group
+		self.gSize = gSize
+		self.gW = gW
+		self.rep = rep
 
 
