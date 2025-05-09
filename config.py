e4e58d4a4864e257e806beb8beece688e581a185
hadiSahin <81286335+hadiSahin@users.noreply.github.com>
2025-05-09 10:12:41 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,21 @@
-MAX_NODES = 1000
-ROUNDS = 10000
+MAX_NODES = 2500
+ROUNDS = 5000
 MAX_ALGORAND = 50
 # Ratio of sybil
-SYBIL = 30
+SYBIL = .30
 # The cost of maintaining a sybil 
-COST_SPLIT = .001
+# COST_SPLIT = .001 
 # How much will be allocated to splitted stakes
-SPLIT_STAKES = 1
+SYBIL_STAKE = 1
 #probability of audit
-PROBA = 0.001
-REWARD = 5
-RATE = .8
+# PROB_CAUGHT = 0.001 
+REWARD =  5.2 #495.22
+# RATE = 0.8 
 #slash rate 
-SLASH = 0.10 # 0.2
+# SLASH = 0.10 # 0.2 
+PERIOD = 3
+WEALTH_THRESHOLD = 90 #90
+# COST_STAY = 0.0005 
+DECAY_RATE = 0.1
+TAX_RATE = 0.5
+FIRST_SAMPLE_RATE = 0.852
